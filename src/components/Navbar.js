08542b8f9c274768ef717b7003a054f2819d48ad
osman <osman.ahmed.2022o@gmail.com>
2023-02-27 00:45:52 +0200

    edit-2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-// import {  NavLink } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Navbar(){
     return(
